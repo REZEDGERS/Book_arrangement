9b9fdeaa7725b61553b0990b078b758385a78995
边缘坐标 <68896486+Edge-coordinates@users.noreply.github.com>
2022-10-03 20:16:10 +0800

    20221003

--- process/对于a无趣的处理.py
+++ process/对于a无趣的处理.py
@@ -11,6 +11,10 @@ fapath = "../.origin"
 gpath = "../YY向"
 
 
+replace_ = ["绿nu", "真-nu才", "自lv", "lv文", "心理BT", "基佬文", "绿文", "ying视", "后gong", "自绿"]
+_replace = ["绿", "男主奴才", "绿", "绿", "心里变态", "gay", "绿", "影视", "后宫", "绿"]
+_not_it = [r"^划到", r"^膈应"]
+
 def dev(filepath):
     TFile = open(filepath, 'r', encoding='utf-8')  # 打开文件(只读)
     content = TFile.readlines()  # 按行读取
@@ -22,11 +26,12 @@ def dev(filepath):
     lfpath = '' # 具体目录是推书还是排雷
     con = "\n> 内容整理自： 微信公众号-纯爱后宫小说" # 版权
     tmfm = [] # front-matter
-    reg = [r"\*\*[一二两三四五六七八九十○零百千0-9１２３４５６７８９０]+.*", r"^分类", "分类"]
+    reg = {'序号' : r"\*\*[一二两三四五六七八九十○零百千0-9１２３４５６７８９０]+.*", '分隔符' : "[:/\\;；,，\(（\[]"}
+
     ts = [''] # 分类
     for i in range(len(content)):
-        line = content[i]
-        if re.search(reg[0], line):
+        line = content[i] 
+        if re.search(reg['序号'], line):
             r = i + 2
             if r != l:
                 fcontent = ''.join(tmfm) + "# " + fname + "\n" +''.join(content[l + 2: r - 2]) + con
@@ -34,6 +39,8 @@ def dev(filepath):
                 tf = open(gpath + '/' + lfpath + '/' + fname +'.md', 'w', encoding='utf-8')
                 tf.writelines(fcontent)
                 tf.close()
+            # 初始化
+            flag = 0
             fname = content[r].strip("* \n").replace(":","：").replace("/","&")
             tmfm = ["---\n", "title: " + fname + "\n", "categories:\n", "- YY向\n", "tags:\n", "---\n"]
             ts = []
@@ -43,17 +50,21 @@ def dev(filepath):
                 lfpath = "扫书"
             tmfm.insert(tmfm.index("tags:\n") + 1, "- " + lfpath + "\n")
             l = r
-        if re.search(reg[1], line.strip()):
-            ts = line.replace(reg[2], "", 1)
-            if ts.find('/') != -1:
-                ts = ts.split('/')
-            elif ts.find('，') != -1:
-                ts = ts.split('，')
+        if re.search(r"^分类", line.strip()):
+            for j in range(len(replace_)):
+                line = line.replace(replace_[j], _replace[j])
+            ts = re.split(reg["分隔符"], line.replace("分类", "", 1).strip(" :："))
             if type(ts) == type([]):
                 for sta in ts:
-                    tmfm.insert(tmfm.index("tags:\n") + 1, "- " + sta.strip("\n :：（）?？+")+ "\n")
+                    for rega in _not_it:
+                        if re.search(rega, sta): flag = 1
+                    if not flag:
+                        tmfm.insert(tmfm.index("tags:\n") + 1, "- " + sta.strip("\n :：（）?？+")+ "\n")
             elif type(ts) == type(""):
-                tmfm.insert(tmfm.index("tags:\n") + 1, "- " + ts.strip("\n :：（）?？+") + "\n")
+                for rega in _not_it:
+                        if re.search(rega, ts): flag = 1
+                if not flag:
+                    tmfm.insert(tmfm.index("tags:\n") + 1, "- " + ts.strip("\n :：（）?？+") + "\n")
     # 单独处理最后一个
     fcontent = ''.join(tmfm) + "# " + fname + "\n" +''.join(content[r + 2:]) + con
     # print(fcontent)
